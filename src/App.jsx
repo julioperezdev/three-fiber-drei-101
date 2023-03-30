13271c7a8e3d93f217c5cd919e55ccd6247c574f
Julio Perez <julioperez.dev@gmail.com>
2023-03-29 23:03:11 -0300

    code of video 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+
+  return (
+    <Canvas id='three-canvas-container'>
+      <Suspense fallback={null}>
+        <Three/>
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default App
---
+++ src/component/three/index.jsx
@@ -0,0 +1,9 @@
+import {angleToRadians} from "../../utils/angle";
+
+export default function Three(){
+    return(
+        <>
+            <ambientLight args={["#ffffff", 1]} />
+        </>
+    )
+}
