3f3d9fe0e8bcc82f8b6b1529d05164c3002758c1
Julio Perez <julioperez.dev@gmail.com>
2023-03-30 17:26:19 -0300

    code of video 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber'
+import Three from "./component/three"
+
+import "./App.css"
 
 function App() {
-
   return (
-    <Canvas id='three-canvas-container'>
+    <Canvas id="three-canvas-container">
       <Suspense fallback={null}>
         <Three/>
       </Suspense>
--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,8 +1,24 @@
+import {PerspectiveCamera} from "@react-three/drei"
 import {angleToRadians} from "../../utils/angle";
 
-export default function Three(){
+export default function Three() {
     return(
         <>
+            <PerspectiveCamera makeDefault position={[0,1,5]} />
+
+            {/* Ball */}
+            <mesh position={[0,0.5,0]}>
+                <sphereGeometry args={[0.5, 32, 32]} />
+                <meshStandardMaterial color="#ffffff" />
+            </mesh>
+
+            {/* floor */}
+            <mesh rotation={[-(angleToRadians(90)),0,0]}>
+                <planeGeometry args={[7,7]}/>
+                <meshStandardMaterial color="green" />
+            </mesh>
+
+            {/* ambientLight */}
             <ambientLight args={["#ffffff", 1]} />
         </>
     )
