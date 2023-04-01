cb185c1a795f676468ff5bc76bd4c03503fc6b27
Julio Perez <julioperez.dev@gmail.com>
2023-04-01 13:42:03 -0300

    Camera & OrbitControls

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,10 +1,37 @@
-import {PerspectiveCamera} from "@react-three/drei"
+import {useRef, useEffect} from "react"
+import {PerspectiveCamera, OrbitControls} from "@react-three/drei"
+import {useFrame} from "@react-three/fiber"
 import {angleToRadians} from "../../utils/angle";
 
 export default function Three() {
+
+    const orbitControlsRef = useRef(null);
+    useFrame((state) => {
+        if(!!orbitControlsRef.current){
+            const {x,y} = state.mouse;
+            const maxAngleThatMouseCanBeRotateHorizontaly = (-x * angleToRadians(45))
+            const maxAngleThatMouseCanBeRotateVerticaly = ((y + 1.5) * angleToRadians(90 - 30))
+            orbitControlsRef.current.setAzimuthalAngle(maxAngleThatMouseCanBeRotateHorizontaly);
+            orbitControlsRef.current.setPolarAngle(maxAngleThatMouseCanBeRotateVerticaly);
+            orbitControlsRef.current.update();
+        }
+    })
+
+    useEffect(() => {
+      if(!!orbitControlsRef.current){
+        console.log(orbitControlsRef.current)
+      }
+    }, [orbitControlsRef.current])
+    
+
     return(
         <>
+            {/* camera */}
             <PerspectiveCamera makeDefault position={[0,1,5]} />
+            <OrbitControls 
+            ref={orbitControlsRef}
+            minPolarAngle={angleToRadians(60)}
+            maxPolarAngle={angleToRadians(80)}/>
 
             {/* Ball */}
             <mesh position={[0,0.5,0]}>
